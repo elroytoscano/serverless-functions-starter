de7f64fc785c497fc86bdeecc0318555bfe53b2a
Elroy Toscano <43146754+elroytoscano@users.noreply.github.com>
2022-02-02 18:51:39 +0530

    Added CORS fix

--- functions/2-api.js
+++ functions/2-api.js
@@ -2,6 +2,9 @@ const items = require('../assets/data')
 
 const handler = async (async, context) => {
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   }
